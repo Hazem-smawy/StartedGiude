a5676b454a64f65988f5a9f06459d397a27e77fc
hazem smawy <hazemsmawy@gmail.com>
2022-08-27 15:30:05 +0300

    Initial Commit

---
+++ StartedGiude/StartedGiudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartedGiudeApp.swift
+//  StartedGiude
+//
+//  Created by mahmood on 29/01/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct StartedGiudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
